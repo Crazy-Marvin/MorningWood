d4046c2e1eb2778dd57b1d1392fa535d976a7941
Marvin <marvin@poopjournal.rocks>
2022-10-04 20:54:33 +0200

    Created morning_wood Using Very Good CLI 0.8.0

--- morning_wood/android/app/src/main/kotlin/rocks/poopjournal/morning_wood/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package rocks.poopjournal.morning_wood
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ morning_wood/android/app/src/main/kotlin/rocks/poopjournal/morningwood/MainActivity.kt
@@ -0,0 +1,6 @@
+package rocks.poopjournal.morningwood.morning_wood
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
